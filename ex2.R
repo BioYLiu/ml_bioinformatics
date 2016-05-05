10aaffcbec32b7176714200184d33bce0a94d9eb
Omar Gutiérrez <omar.vpa@gmail.com>
2016-05-05 18:08:24 +0200

    Update second part, explanation and code

--- ex2.R
+++ ex2.R
@@ -81,44 +81,65 @@ dev.off()
 # A-i
 dat = read.csv("exercise2-2.csv")
 
-meanByCol = apply(dat,2,mean)
+# Here we calculate the mean and the variance for each colum in the dat dataset
+meanByCol = apply(dat, 2, mean)
 varByCol = apply(dat, 2, var)
+sdByCol = apply(dat, 2, sd)
 
 meanByCol
 varByCol
 # A-ii
-cor(dat$x1, dat$y1)
-cor(dat$x2, dat$y2)
-cor(dat$x3, dat$y3)
-cor(dat$x4, dat$y4)
+# calculate correlation between X and Y.
+c1 = cor(dat$x1, dat$y1)
+c2 = cor(dat$x2, dat$y2)
+c3 = cor(dat$x3, dat$y3)
+c4 = cor(dat$x4, dat$y4)
 
 # A-iii
-# b= pearson * sqrt(var x)/sqrt(var y)
-# a= mean(y) - b*mean(x)
-b1 <- cor(dat$x1, dat$y1) * sqrt(varByCol[1]) / sqrt(varByCol[2])
+# Here calculate the values for a and b, that will be used later to draw the regression line
+b1 <- c1 * sdByCol[1] / sdByCol[2]
+#b1 <- 0.5
 a1 <- meanByCol[2] - (b1 * meanByCol[1])
-b2 <- cor(dat$x2, dat$y2) * sqrt(varByCol[3]) / sqrt(varByCol[4])
+
+b2 <- c2 * sqrt(varByCol[3]) / sqrt(varByCol[4])
+#b2 = 0.5
 a2 <- meanByCol[4] - (b1 * meanByCol[3])
-b3 <- cor(dat$x3, dat$y3) * sqrt(varByCol[5]) / sqrt(varByCol[6])
+
+b3 <- c3 * sqrt(varByCol[5]) / sqrt(varByCol[6])
+#b3 <- 0.5
 a3 <- meanByCol[6] - (b1 * meanByCol[5])
-b4 <- cor(dat$x4, dat$y4) * sqrt(varByCol[7]) / sqrt(varByCol[8])
+
+b4 <- c4 * sqrt(varByCol[7]) / sqrt(varByCol[8])
+#b4 <- 0.5
 a4 <- meanByCol[8] - (b1 * meanByCol[7])
 
 
 #==================================================
 #B
-plot(dat$x1, dat$y1, xlim=c(0, 20), ylim=c(-5,15))
+
+# With color red we draw a line using the equation line.
+# With color blue we draw a line using the `lm` function provide by R.
+
+# We can see that the case 3 is the data which better fits the regression line. Almost perfect. But it does exist an outlier value. 
+
+# If we compare the scatterplots and the correlation value that we calculated before we can check that...
+# 1. we have a very well distributed data, and a normal distribution.
+# 2. we can see a relationship between X and Y, this one is a functional relationship.
+# 3. we have a linear relationship between X and Y.
+# 4. we can see that is not a linear relationship, the outlier modifies markedly the regression line.
+
+plot(dat$x1, dat$y1, xlim=c(0, 20), ylim=c(-5,15), xlab="X1", ylab="Y1", main="X1-Y1 scatterplot")
 abline(a=a1, b=b1, col="red")
-abline(lm(dat$y1~dat$x1), col="blue")
+#abline(lm(dat$y1~dat$x1), col="blue")
 
-plot(dat$x2, dat$y2, xlim=c(0, 20), ylim=c(-5,15))
+plot(dat$x2, dat$y2, xlim=c(0, 20), ylim=c(-5,15), xlab="X2", ylab="Y2", main="X2-Y2 scatterplot")
 abline(a=a2, b=b2, col="red")
-abline(lm(dat$y2~dat$x2), col="blue")
+#abline(lm(dat$y2~dat$x2), col="blue")
 
-plot(dat$x3, dat$y3, xlim=c(0, 20), ylim=c(-5,15))
+plot(dat$x3, dat$y3, xlim=c(0, 20), ylim=c(-5,15), xlab="X3", ylab="Y3", main="X3-Y3 scatterplot")
 abline(a=a3, b=b3, col="red")
-abline(lm(dat$y3~dat$x3), col="blue")
+#abline(lm(dat$y3~dat$x3), col="blue")
 
-plot(dat$x4, dat$y4, xlim=c(0, 20), ylim=c(-5,15))
+plot(dat$x4, dat$y4, xlim=c(0, 20), ylim=c(-5,15), xlab="X4", ylab="Y4", main="X4-Y4 scatterplot")
 abline(a=a4, b=b4, col="red")
-abline(lm(dat$y4~dat$x4), col="blue")
+#abline(lm(dat$y4~dat$x4), col="blue")
